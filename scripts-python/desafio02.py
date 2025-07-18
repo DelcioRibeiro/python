a905ae58e724643c4ffe66d551f8b371209f31a6
Delcio  Ribeiro <delcioribeiro97@gmail.com>
2025-07-17 22:19:27 -0300

    primeiros passos em python

---
+++ scripts-python/desafio02.py
@@ -0,0 +1,5 @@
+print('===== DESAFIO 02 =====')
+dia = input('Dia de nascimento?')
+mes = input('Mes de nascimento?')
+ano = input('Ano de nascimento?')
+print('Você nasceu no dia ',dia,' de ',mes,' de ',ano,'. Correto?') 
---
+++ scripts-python/desafio03.py
@@ -0,0 +1,5 @@
+print('===== DESAFIO 03 =====')
+num01 = int(input ('Digite um numero!'))
+num02 = int(input ('Digite outro numero!'))
+soma = num01 + num02
+print('A soma de ',num01,' e ',num02,' e de ',soma,'!')
