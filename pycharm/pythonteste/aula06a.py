837d00a77dd670034c254a2ed69cb7ea45db13eb
Delcio  Ribeiro <delcioribeiro97@gmail.com>
2025-07-21 21:33:18 -0300

    primeiros desafios

---
+++ pycharm/pythonteste/aula06a.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite o valor: '))
+n2 = int(input('Digite outro numero:'))
+s = n1 + n2
+#print('A soma de ', n1, 'e', n2, 'vale', s)
+print('A soma de {0} e {1} vale {2}'.format(n1, n2, s))
